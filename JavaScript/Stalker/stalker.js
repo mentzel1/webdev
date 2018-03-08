167c433c26c47cb8672431283915de9076e8b418
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-08 14:37:22 -0600

    Completed Javascript excercise

---
+++ JavaScript/Stalker/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Hello, what is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("The user's name is " + firstName + " " + lastName);
+console.log("The user's age is: " + age);
