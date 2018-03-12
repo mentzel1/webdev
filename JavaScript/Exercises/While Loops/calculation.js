5100184c5750c250610ddca32991f4d5b0105f5f
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-12 11:58:28 -0500

    Completed For Loops Excercise

--- JavaScript/Exercises/While Loops/calculation.js
+++ JavaScript/Exercises/While Loops/calculation.js
@@ -1,1 +1,35 @@
-alert("HELLO!");
+// alert("HELLO!");
+
+// Print all the numbers between -10 and 19
+var count = -10;
+while(count <20){
+  console.log(count);
+  count++;
+}
+
+// Print all the even numbers between 10 and 40
+count = 10;
+while(count <= 40){
+  if(count%2 === 0){
+    console.log(count);
+  }
+  count++;
+}
+
+// Print all the odd numbers between 300 and 333
+count = 300;
+while(count <=333){
+  if(count%2 !== 0){
+    console.log(count);
+  }
+    count++;
+}
+
+// Print all numbers divisible by 5 and 3 between 5 and 50
+count = 5;
+while(count <= 50){
+  if(count%5 === 0 && count%3 === 0){
+    console.log(count);
+  }
+    count++;
+}
