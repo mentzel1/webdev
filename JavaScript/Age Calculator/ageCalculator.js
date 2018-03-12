e412883d8fa8b1e2a9d0fed760b1c0128ea04a86
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-12 17:19:17 -0500

    Fixed issue where javacript ran before html loaded on page

--- JavaScript/Age Calculator/ageCalculator.js
+++ JavaScript/Age Calculator/ageCalculator.js
@@ -1,4 +1,6 @@
-var age = prompt("Please enter your current age:");
- age = 365*age;
+window.setTimeout(function() {
+  var age = prompt("Please enter your current age:");
+   age = 365*age;
 
-alert("You have been alive " + age + " days!");
+  alert("You have been alive " + age + " days!");
+}, 500);
