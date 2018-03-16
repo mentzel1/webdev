531a025256bf581c9e4ecd277975314a4c0147ef
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-16 17:24:24 -0500

    Figured out why you had to use this, noted in comments

--- JavaScript/Exercises/Todo EventListener/todo.js
+++ JavaScript/Exercises/Todo EventListener/todo.js
@@ -4,7 +4,7 @@ var li_arr = document.getElementsByTagName("li");
 
 // You cannot do the following:
 //   li_arr[i].classList.add("hover")
-// Becuase the for loop only exexcutes onse and at the end of it i = 4 and therfore li_arr[4] is undefined. The anonomyous function is triggered when we mouseOver something. So "this" works because it refers to the current li that we are hovering over and thus we use that when executing our funcition
+// Becuase the for loop only exexcutes once and at the end of it i = 4 and therfore li_arr[4] is undefined. The anonomyous function is triggered when we mouseOver something. So "this" works because it refers to the current li that we are hovering over and thus we use that when executing our funcition
 for(var i = 0; i < li_arr.length; i++){
   console.log(li_arr[i]);
   li_arr[i].addEventListener("mouseover", function(){
