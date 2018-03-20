ddcdd2e61dc521f42a32358112909743ab5f5198
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-20 10:48:53 -0500

    Cleaned up code

--- JavaScript/RGB Game/colorGame.js
+++ JavaScript/RGB Game/colorGame.js
@@ -12,51 +12,42 @@ var numGame = numHard;
 var pickedColor;
 
 //Initialize game with reset function
-resetGame();
+initGame();
 
-// //Set Random colors
-// randSquareColors(numGame);
-// //Cant pick random squarecolor until you set all the random squares with colors
-// var pickedColor= colorPicker(numHard);
-//
-// //Update the h1 to display the color in rgb
-// colorDisplay.textContent = pickedColor.toUpperCase();
 
 // Add event listeners
 for(var i=0; i<squares.length; i++){
   squares[i].addEventListener("click", clickedSquare);
 }
-reset.addEventListener("click", resetGame);
+reset.addEventListener("click", initGame);
 hard.addEventListener("click", function(){
-  hard.classList.add("selected");
-  easy.classList.remove("selected");
   numGame = numHard;
-  resetDifficulty(numGame);
+  resetGame(numGame);
 })
 easy.addEventListener("click", function(){
-  hard.classList.remove("selected");
-  easy.classList.add("selected");
   numGame = numEasy;
-  resetDifficulty(numGame);
+  resetGame(numGame);
 })
 
-function resetDifficulty(num){
-  randSquareColors(num);
-  pickedColor= colorPicker(num);
+function resetGame(gameType){
+  if(gameType === numEasy){
+    hard.classList.remove("selected");
+    easy.classList.add("selected");
+  }else{
+    hard.classList.add("selected");
+    easy.classList.remove("selected");
+  }
+  randSquareColors(gameType);
+  pickedColor= colorPicker(gameType);
   colorDisplay.textContent = pickedColor.toUpperCase();
   bannerDisplay.style.backgroundColor = "#538CC6";
   resultsDisplay.textContent = " ";
+  reset.textContent = "NEW COLORS";
 }
 
-function resetGame(){
-  hard.classList.add("selected");
-  easy.classList.remove("selected");
-  randSquareColors(numGame);
-  reset.textContent = "NEW COLORS";
-  pickedColor= colorPicker(numHard);
-  colorDisplay.textContent = pickedColor.toUpperCase();
-  bannerDisplay.style.backgroundColor = "#538CC6";
-  resultsDisplay.textContent = " ";
+//Initializes the game using reset
+function initGame(){
+  resetGame(numGame);
 }
 
 //Generates Specified number of colored squares
@@ -87,23 +78,18 @@ function randColor(){
   return color;
 }
 
-// Method checks if clicked square is correct solution, if not, removes square by setting background to same color as the body. If it is correct, Shows "Correct!" on banner and resets game
+// Method checks if clicked square is correct solution, if not, removes square by setting background to same color as the body. If it is correct, Shows "Correct!" on banner, then banner color and all other sqaures become that color. Game ends, user can play again by clicking buttons
 function clickedSquare(){
   console.log("clicked square background color" + this.style.backgroundColor);
   if( this.style.backgroundColor === pickedColor){
-    correct(pickedColor);
+    for(var i=0; i<numGame; i++){
+      squares[i].style.backgroundColor = pickedColor;
+    }
+    resultsDisplay.textContent = "Correct!";
+    reset.textContent = "PLAY AGAIN?"
+    bannerDisplay.style.backgroundColor = pickedColor;
   }else{
     this.style.backgroundColor = "#14141f";
     resultsDisplay.textContent = "Try Again";
   }
-}
-
-//If user selects correct color, then banner color and all other sqaures become that color
-function correct(pickedColor){
-  for(var i=0; i<numGame; i++){
-    squares[i].style.backgroundColor = pickedColor;
-  }
-  resultsDisplay.textContent = "Correct!";
-  reset.textContent = "PLAY AGAIN?"
-  bannerDisplay.style.backgroundColor = pickedColor;
 }
