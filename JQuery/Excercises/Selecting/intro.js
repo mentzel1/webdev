da44cf9a8ff79d1aa69529bce77417e8358f5378
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-03-22 12:08:24 -0500

    Completed excercise on selecting with jquery

---
+++ JQuery/Excercises/Selecting/intro.js
@@ -0,0 +1,6 @@
+$("div").css("background", "purple");
+//we are selecting a div with class of highlight. NO SPACES
+$("div.highlight").css("width", "200px");
+$("#third").css("border", " 1px solid orange");
+// $("h1 + div").css("color", "pink");
+$("div:first-of-type").css("color", "pink");
