2a9f200e4de76fce8fce3a7cd79adc42fa4b4863
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-06-15 11:36:06 -0500

    Completed routes and confirmed working

--- backEnd/expressApp2/app.js
+++ backEnd/expressApp2/app.js
@@ -6,16 +6,35 @@ app.get("/", function(req, res){
 });
 
 app.get("/speak/:animal", function(req, res){
-  var animal = req.params.animal;
-  //console.log(req);
-
-  if(animal == "pig"){
-    res.send("The pig says'Oink'");
-  }else if( animal == "cow"){
-    res.send("The pig says 'Moo'");
-  }else if( animal == "dog"){
-    res.send("The pig says 'Woof Woof!'");
+  var animal = req.params.animal.toLowerCase();
+  var sounds = {
+    pig: "Oink",
+    cow: "Moo",
+    dog: "Woof Woof!",
+    fish: "..."
+  };
+  console.log(sounds[animal]);
+  if(sounds[animal] == undefined){
+    res.send("Sorry, page not found....What are you doin gwith your life?");
+  }else{
+    res.send("The " + animal + " says '" + sounds[animal] + "'");
   }
+});
+
+app.get("/repeat/:word/:val", function(req, res){
+  var num = Number(req.params.val);
+  var str = req.params.word;
+  var strFinal = " ";
+
+  while(num > 0){
+    strFinal += " " + str + " ";
+    num--;
+  }
+  res.send(strFinal);
+});
+
+app.get("*", function(req, res){
+  res.send("Sorry, page not found....What are you doin gwith your life?");
 });
 
 app.listen(3000, function(){
