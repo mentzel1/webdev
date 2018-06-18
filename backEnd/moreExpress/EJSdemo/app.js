91a8db91db0c367fc3736504437d9790db0014a9
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-06-18 13:17:16 -0500

    included public folder and set express to view all files as ejs unless otherwise states

--- backEnd/moreExpress/EJSdemo/app.js
+++ backEnd/moreExpress/EJSdemo/app.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var app = express();
 
+//Need to tell express to explicitly serve all directories (other than default views) in order for it to be found
+app.use(express.static("public"));
+
+//We can specify that all are view files will be ejs, so we do not need to include the extension, just the name of the files ("home.ejs" vis "home")
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
   var thing = req.params.thing;
-  res.render("love.ejs", {thingVar: thing});
+  res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -16,7 +22,7 @@ app.get("/posts", function(req, res){
     {title: "Hipster near the woods", author: "Dana Vuderburg" },
     {title: "Dr. Who went where?", author: "Chris Butterburg"}
   ];
-  res.render("posts.ejs", {posts: posts});
+  res.render("posts", {posts: posts});
 });
 
 app.listen(3000, function(){
