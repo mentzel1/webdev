de102aa71942d80d01d1812d1c358825f24aeeb7
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-06-19 08:43:08 -0500

    Modified to use Yahoo weather API to print hawaii sunset time

--- backEnd/APIs/first_request.js
+++ backEnd/APIs/first_request.js
@@ -1,12 +1,19 @@
 var request = require("request");
-request("http://autocomplete.wunderground.com/aq?query=San%20F", function(error, response, body){
+request("https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(error, response, body){
   if(error){
     console.log("ERROR!!");
     console.log("error:", error);
   }else{
     if(response.statusCode==200){
+      //By default results from web APIs are strings
+      console.log("type: " + typeof body);
       console.log("status code:", response.statusCode);
-      console.log("body:", body);
+      //You can convert the json string to an object using built in parse function
+      var dataObj = JSON.parse(body);
+      console.log("New Type: " + typeof dataObj);
+      console.log("body:", dataObj);
+
+      console.log("Sunset time in Hawaii is at: " + dataObj.query.results.channel.astronomy.sunset);
     }
   }
 });
