7c45ff54e5f076ebf8582cf7dd9bdbed61c7bc30
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-06-21 16:49:28 -0500

    Customized form page, created navbar but broken collapse

--- backEnd/YelpCamp/v1/app.js
+++ backEnd/YelpCamp/v1/app.js
@@ -5,14 +5,18 @@ var bodyParser = require("body-parser");
 //parses url encoded data, needed for forms
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Do not require ejs extension in file names
+app.set("view engine", "ejs");
+
+
 var campgrounds = [
+  {name: "Smokey Mountains", img:"https://www.rei.com/adventures/assets/adventures/images/trip/core/northamerica/smw_hero"},
+  {name: "Estes Park", img: "https://43mbhp3aft5g3uc0tuhsk4a8-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/00-Sky-Pond-in-Rocky-Mountain-National-Park-bratman-creative-commons.jpg"},
+  {name: "Yosemite", img: "https://e794d552b4c822b8205c-27b9cc3fb8731a4a7598943b8a8a6a91.ssl.cf1.rackcdn.com/15/1/large.jpg"},
   {name: "Smokey Mountains", img:"https://www.rei.com/adventures/assets/adventures/images/trip/core/northamerica/smw_hero"},
   {name: "Estes Park", img: "https://43mbhp3aft5g3uc0tuhsk4a8-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/00-Sky-Pond-in-Rocky-Mountain-National-Park-bratman-creative-commons.jpg"},
   {name: "Yosemite", img: "https://e794d552b4c822b8205c-27b9cc3fb8731a4a7598943b8a8a6a91.ssl.cf1.rackcdn.com/15/1/large.jpg"}
 ];
-
-//Do not require ejs extension in file names
-app.set("view engine", "ejs");
 
 //Main page
 app.get("/", function(req, res){
@@ -22,6 +26,23 @@ app.get("/", function(req, res){
 //Campground listing Page
 app.get("/campgrounds", function(req, res){
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+});
+
+app.post("/campgrounds", function(req, res){
+  //Store new campground information
+  var campName = req.body.name;
+  var campUrl = req.body.img;
+  var newCamp = {
+    name: campName,
+    img: campUrl
+  };
+  campgrounds.push(newCamp);
+  //redirect is get by default
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000, function(){
