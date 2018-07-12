9cef169cbcc6776a2dd170290fa7ef276baee626
mentzel1 <31321535+mentzel1@users.noreply.github.com>
2018-07-12 16:27:25 -0500

    Added registration page and routes, completed app

--- backEnd/Authentication/app.js
+++ backEnd/Authentication/app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     mongoose = require("mongoose"),
-    bodyparser = require("body-parser"),
+    bodyParser = require("body-parser"),
     passport = require("passport"),
     localStrategy = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose"),
@@ -19,8 +19,10 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 //We do not have to write our own methods to encode our session ID info and decode it because we are getting it from passportLocalMongoose we pluged into user shcema
-passport.serializeUser(User.serializeUser()):
-passpoer.deserializeUser(User.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//Telling passport that for the local strategy, use that version of user.authenticate
+passport.use(new localStrategy(User.authenticate()));
 
 //Set default file extension to use
 app.set("view engine", "ejs");
@@ -28,13 +30,70 @@ app.set("view engine", "ejs");
 //Connect to mongo database
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
+//parses url encoded data, needed for forms
+app.use(bodyParser.urlencoded({extended: true}));
+
+//====================================
+//ROUTES
+//====================================
 app.get("/", function(req, res){
   res.render("index");
 });
 
-app.get("/secret", function(req, res){
+//Show page that requires Authentication
+app.get("/secret", isLoggedIn,function(req, res){
   res.render("secret");
 });
+
+//AUTHENTICATION ROUTES
+//Show sign up form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+//Store credentials from sign up page in database
+app.post("/register", function(req, res){
+  // console.log(req.body.username);
+  // console.log(req.body.password);
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }else{
+      //Logs user in, takes care of session, will run serializeUser method
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secret");
+      });
+    }
+  })
+});
+
+//LOGIN ROUTES
+//Shows login page for user
+app.get("/login", function(reg, res){
+  res.render("login");
+});
+//Check users credentials using authenticate function (used here as MIDDLEWARE) before we reach end of route signified by our callback function
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect:"/login"
+}), function(req, res){
+});
+
+//LOGOUT ROUTE
+app.get("/logout", function(req, res){
+  //All user data is erased from this session by passport?
+  req.logout();
+  res.redirect("/");
+});
+
+//Function which checks if user is logged in. This is MIDDLEWARE!!!
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect("/login");
+  }
+}
 
 app.listen(3000, function(){
   console.log("Authentication server is starting!");
--- backEnd/Authentication/models/user.js
+++ backEnd/Authentication/models/user.js
@@ -7,6 +7,7 @@ var userSchema = new mongoose.Schema({
 });
 
 //Provides extra methods to serialize, deserialize a user
+//This takes care of the hashing/salting of users password
  userSchema.plugin(passportLocalMongoose);
 
 module.exports =  mongoose.model("user", userSchema);
