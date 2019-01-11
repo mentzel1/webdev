51d187a3c04040e1ee2d2aafcd0b1df44ae5dc7d
mentzel1 <mentzel1@illinois.edu>
2019-01-11 11:10:32 -0600

    removed app since became a bigger project

--- backEnd/BlogApp/models/comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var commentSchema = new mongoose.Schema({
-  author: {},
-  body: String,
-  date: {type: Date, default: Date.now}
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
